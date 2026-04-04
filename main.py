a28bec6205a11f0fcef0313be51fb5173a22343a
GIYO-CODER <giyo20061@gmail.com>
2026-04-04 11:05:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1010,7 +1010,7 @@ def handle_symbol(pair):
             logger.info(f"{symbol} | Daily bias does not allow BUY, skipping")
             return
             
-        if daily_fvg_state[symbol]["allow_buy"]:
+        if last_closed["close"] > bf["high"]):
             entry = last_closed["close"]
             
             deep = bf["deepest_touch"]
@@ -1157,7 +1157,7 @@ def handle_symbol(pair):
             logger.info(f"{symbol} | Daily bias does not allow SELL, skipping")
             return
             
-        if daily_fvg_state[symbol]["allow_sell"]:
+        if last_closed["close"] < sf["low"]):
             entry = last_closed["close"]
             
             deep = sf["deepest_touch"]
